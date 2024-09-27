102202951d09ef22c4687651df582bde61f971f7
ipeeka <pkbrat424@gmail.com>
2024-09-27 14:50:12 +0530

    new files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ import { GuidesComponent } from './guides/guides.component';
 import { RatingsComponent } from './ratings/ratings.component';
 import { BookingComponent } from './booking/booking.component';
 import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { DestinationsComponent } from './destinations/destinations.component';
+import { ServicesComponent } from './services/services.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -14,6 +18,10 @@ export const routes: Routes = [
   { path: 'ratings', component: RatingsComponent },
   { path: 'booking', component: BookingComponent },
   { path: '', component: HomeComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'blogs', component: BlogsComponent },
+  { path: 'destinations', component: DestinationsComponent },
+  { path: 'services', component: ServicesComponent },
 ];
 
 @NgModule({
