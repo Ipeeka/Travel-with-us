6a05daaf9fd5533e9d48dbebd86fd9614f227209
ipeeka <pkbrat424@gmail.com>
2024-10-01 15:24:55 +0530

    slight changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RegisterComponent , RouterLink ],
+  imports: [RegisterComponent , RouterLink  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   constructor(private router: Router) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  signupUsers: any[] = [];
+  signupObj:any = {
+    username: '',
+    password: ''
+  };
+
+
+
+  
+
+  navigateToRegister() {
+    this.router.navigate(['/register']);
+  }
 
   onSubmit() {
     // Add login logic here
     console.log('Login submitted');
-  }
-
-  navigateToRegister() {
-    this.router.navigate(['/register']);
   }
 }
